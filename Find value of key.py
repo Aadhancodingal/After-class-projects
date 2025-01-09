690531a4f243e787a57a41caaae090d2850ae94b
Aadhancodingal <kcmharish@gmail.com>
2025-01-09 21:49:20 +0530

    initial commit

---
+++ Find value of key.py
@@ -0,0 +1,56 @@
+key_dict = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26,}
+key = str(input("Enter a key"))
+if(key == "A","a"):
+ print(key_dict["A"])
+elif(key == "B","b"):
+ print(key_dict["B"])
+elif(key == "C","c"):
+ print(key_dict["C"])  
+elif(key == "D","d"):
+ print(key_dict["D"])
+elif(key == "E","e"):
+ print(key_dict["E"])
+elif(key == "F","b"):
+ print(key_dict["F"])
+elif(key == "G","b"):
+ print(key_dict["G"])
+elif(key == "H","h"):
+ print(key_dict["H"])
+elif(key == "I","i"):
+ print(key_dict["I"]) 
+elif(key == "J","j"):
+ print(key_dict["J"])
+elif(key == "K","k"):
+ print(key_dict["K"])
+elif(key == "L","l"):
+ print(key_dict["L"])
+elif(key == "M","m"):
+ print(key_dict["M"])
+elif(key == "N","n"):
+ print(key_dict["N"])
+elif(key == "O","o"):
+ print(key_dict["O"])
+elif(key == "Q","q"):
+ print(key_dict["Q"])
+elif(key == "R","r"):
+ print(key_dict["R"])
+elif(key == "S","s"):
+ print(key_dict["S"])         
+elif(key == "T","t"):
+ print(key_dict["T"])
+elif(key == "U","u"):
+ print(key_dict["U"])
+elif(key == "V","v"):
+ print(key_dict["V"])
+elif(key == "W","w"):
+ print(key_dict["W"])
+elif(key == "X","x"):
+ print(key_dict["X"])
+elif(key == "Y","y"):
+ print(key_dict["Y"])
+else:
+ print(key_dict["Z"])      
+  
+
+     
+
